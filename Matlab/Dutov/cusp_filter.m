b680c7a6f939fabd8facecb5affe3e6225d11d85
idutoff <idutoff@gmail.com>
2024-09-23 22:07:04 +0700

    Задание 1

---
+++ Matlab/Dutov/cusp_filter.m
@@ -0,0 +1,39 @@
+function [s] = cusp_filter(v, l, k,m1,m2)
+
+    N = length(v); %оперделяем длинну массива
+    d_k = zeros(1, N);
+    d_1 = zeros(1, N);
+    p = zeros(1, N);
+    q = zeros(1, N);
+    s = zeros(1, N);
+
+    p(1) = 0; %определяем начальные (первые) элементы массивов
+    q(1) = 0;
+    s(1) = 0;
+
+    for n = 2:N %начинаем со второго элемента т.к. первый определен
+        
+        if n > k && n > l && n > k + l
+            d_k(n) = v(n) - v(n - k); %далее все if/else для начальных элементов массивов.
+        else
+            d_k(n) = v(n); 
+        end
+        
+        if n > 1
+            d_1(n) = v(n) - v(n - 1);
+        else
+            d_1(n) = v(n);
+        end
+        
+         if n > l
+            p(n) = p(n-1) + d_k(n) - k * d_1(n-l);
+        else
+            p(n) = p(n-1) + d_k(n);  
+        end
+      
+        q(n) = q(n - 1) + m2 * p(n);
+        
+        s(n) = s(n - 1) + q(n) + m1 * p(n);
+
+    end
+end
