c945a92859c7ca5e37b932400643dd39c0fe8201
Vladimiir73 <zhivotiagin.vova@gmail.com>
2024-09-28 22:39:39 +0700

    исправленное задание 2

--- Quartus II/zadanie_2/zhivotiagin/Register.sv
+++ Quartus II/zadanie_2/zhivotiagin/Register.sv
@@ -6,9 +6,20 @@ module register (A,B,C,clk,DATA_OUT);
     input logic [WIDTH-1:0] C;    // входной сигнал C
     input logic clk;        // сигнал тактовой частоты 200 ћ√ц
     output logic [OUT_WIDTH-1:0] DATA_OUT; // выходной сигнал
-
+	reg [OUT_WIDTH-1:0] t1;
+	logic [WIDTH-1:0] C_0;
+	logic [WIDTH-1:0] C_1;
     always_ff @(posedge clk) begin
-        DATA_OUT <= A * B + C;
+        t1 <= A * B;
+    end
+    
+    always_ff @(posedge clk) begin
+        C_0 <= C;
+        C_1 <= C_0;
+    end
+    
+    always_ff @(posedge clk) begin
+        DATA_OUT <= t1+C_1;
     end
 endmodule
 
