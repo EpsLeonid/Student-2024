53ef8edfe829062fb803eba717353218e02e0c0f
Alexandr <sanka.kopp@gmail.com>
2024-09-29 17:46:47 +0700

    Загрузил задание 2 повторно
    вся папка целиком

--- Quartus II/zadanie_2/Kopylov/Register_param.sv
+++ Quartus II/zadanie_2/Kopylov/Register_param.sv
@@ -3,17 +3,22 @@
     module Register_2 (
     input logic [WIDTH-1:0] A,       
     input logic [WIDTH-1:0] B,       
-    input logic [WIDTH-1:0] C,       
-    input logic clk,           
+    input logic [WIDTH-1:0] C,      
+    input logic clk,   
+    output logic [OUT_WIDTH-1:0] result1,
+    output logic [OUT_WIDTH-1:0] result2,  
+    output logic [WIDTH-1:0] C_delayed,     
     output logic [OUT_WIDTH-1:0] DATA_OUT
 );
 
-    logic [OUT_WIDTH-1:0] result1;  // Промежуточный результат умножения (16 бит)
-    logic [OUT_WIDTH-1:0] result2;   // Промежуточный результат сложения (16 бит)
+
+	always_ff @(posedge clk) begin
+			C_delayed <= C;               
+		end
 
     always_ff @(posedge clk) begin
         result1 <= A * B;             
-        result2 <= result1 + C;   
+        result2 <= result1 + C_delayed;   
         DATA_OUT <= result2;
     end
     
