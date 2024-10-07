5ea3af0ac496873fa032f513b4e2853009265876
Vladimiir73 <zhivotiagin.vova@gmail.com>
2024-10-07 08:24:16 +0700

    исправленное задание 2

--- Quartus II/zadanie_2/zhivotiagin/Register.sv
+++ Quartus II/zadanie_2/zhivotiagin/Register.sv
@@ -8,19 +8,18 @@ module register (A,B,C,clk,DATA_OUT);
     output logic [OUT_WIDTH-1:0] DATA_OUT; // выходной сигнал
 	reg [OUT_WIDTH-1:0] t1;
 	logic [WIDTH-1:0] C_0;
-	logic [WIDTH-1:0] C_1;
+	logic [OUT_WIDTH-1:0] t2;
+	
+	always_ff @(posedge clk) begin
+        C_0 <= C;
+    end
+    
     always_ff @(posedge clk) begin
         t1 <= A * B;
+        t2 <= t1+C_0;
+        DATA_OUT <= t2;
     end
     
-    always_ff @(posedge clk) begin
-        C_0 <= C;
-        C_1 <= C_0;
-    end
-    
-    always_ff @(posedge clk) begin
-        DATA_OUT <= t1+C_1;
-    end
 endmodule
 
 
