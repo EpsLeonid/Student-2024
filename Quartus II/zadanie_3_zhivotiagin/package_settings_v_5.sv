131e771be410a2f4dee47bf2f194ef18194200c9
Vladimiir73 <zhivotiagin.vova@gmail.com>
2024-10-29 02:01:11 +0700

    zadanie_3 filter
    это был кошмарный кошмар

---
+++ Quartus II/zadanie_3_zhivotiagin/package_settings_v_5.sv
@@ -0,0 +1,14 @@
+`ifndef PARAMS_SV
+`define PARAMS_SV
+
+package package_settings_v_5;
+//-----------------------------------------------------------------------------
+// Parameter Declaration(s)
+//-----------------------------------------------------------------------------
+	parameter int k_v_5 = 6;
+	parameter int l_v_5 = 6;
+	parameter int M_v_5 = 16;
+//-----------------------------------------------------------------------------
+endpackage
+
+`endif // PARAMS_SV
