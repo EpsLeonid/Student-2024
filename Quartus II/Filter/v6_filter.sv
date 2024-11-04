bf0208d8e6e3e9856b979b4376fd4953de6ff2ef
Alexandr <sanka.kopp@gmail.com>
2024-11-04 23:58:31 +0700

    Загрузил 3 задание
    добавил reset. Начинает выдавать значения на 140ns

--- Quartus II/Filter/v6_filter.sv
+++ Quartus II/Filter/v6_filter.sv
@@ -22,21 +22,43 @@ module v6_filter(
 	
 	always_ff @(posedge clk) begin
 		v_v_6[0]<=input_data;
-		for (int i = k_v_6; i >= 1; i--) begin
-			v_v_6[i] <= v_v_6[i-1];
+		
+		if(!reset) begin
+			for (int i = k_v_6; i >= 1; i--) begin
+				v_v_6[i] <= 0;
 		end
+	end
+		
+		else
+			for (int i = k_v_6; i >= 1; i--) begin
+				v_v_6[i] <= v_v_6[i-1];
+		end	
     end
 	
 	always_ff @(posedge clk) begin
-		v_delay_v_6 <= v_v_6[0];
-		d_k_delay_v_6 <= d_k_v_6;
-		p_delay_v_6 <= m1_v_6 * p_v_6;
-	end
+		if(!reset) begin
+				v_delay_v_6 <= 0;
+				d_k_delay_v_6 <= 0;
+				p_delay_v_6 <= 0;
+			end
+		else
+				v_delay_v_6 <= v_v_6[0];
+				d_k_delay_v_6 <= d_k_v_6;
+				p_delay_v_6 <= m1_v_6 * p_v_6;
+		end
 	
 	always_ff @(posedge clk) begin
-		d_1_delay_v_6[0]<=d_1_v_6*k_v_6;
-		for (int i = l_v_6; i >= 1; i--) begin
-			d_1_delay_v_6[i] <= d_1_delay_v_6[i-1];
+    
+    if(!reset) begin
+			d_1_delay_v_6[0]<=d_1_v_6*k_v_6;
+			for (int i = l_v_6; i >= 1; i--) begin
+				d_1_delay_v_6[i] <= 0;
+			end
+		end
+		else
+			d_1_delay_v_6[0]<=d_1_v_6*k_v_6;
+			for (int i = l_v_6; i >= 1; i--) begin
+				d_1_delay_v_6[i] <= d_1_delay_v_6[i-1];
 		end
     end
 	
