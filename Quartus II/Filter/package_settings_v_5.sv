a409bdaa1d8a05283ea06242cbd4808abdbc472f
Vladimiir73 <zhivotiagin.vova@gmail.com>
2024-11-05 00:59:22 +0700

    ToZhE PoMeNiAl PaRaMeTRi

--- Quartus II/Filter/package_settings_v_5.sv
+++ Quartus II/Filter/package_settings_v_5.sv
@@ -1,14 +1,12 @@
-`ifndef PARAMS_SV
-`define PARAMS_SV
+`ifndef PARAMS_V5
+`define PARAMS_V5
 
 package package_settings_v_5;
-//-----------------------------------------------------------------------------
-// Parameter Declaration(s)
-//-----------------------------------------------------------------------------
+
 	parameter int k_v_5 = 6;
 	parameter int l_v_5 = 6;
 	parameter int M_v_5 = 16;
-//-----------------------------------------------------------------------------
+
 endpackage
 
-`endif // PARAMS_SV
+`endif
